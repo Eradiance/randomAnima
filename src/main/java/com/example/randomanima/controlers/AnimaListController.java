750e5e66f7211d56a514e557c3037e389f63c487
ERadince <d.radaev99@gmaail.com>
2023-10-11 23:57:03 +0300

    first commit

--- src/main/java/com/example/randomanima/controlers/AnimaListController.java
+++ src/main/java/com/example/randomanima/controlers/AnimaListController.java
@@ -1,2 +1,12 @@
-package com.example.randomanima.controlers;public class AnimaListController {
+package com.example.randomanima.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AnimaListController {
+    @RequestMapping("/")
+    public String getAnimaList(){
+        return "AnimaList.html";
+    }
 }
